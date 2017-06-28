8807b80286bcde26ad2a9614c31f0a3eaa744d8d
Capblancq <thibaut.capblancq@gmail.com>
2017-06-28 10:35:44 +0200

    Update step_3_Prepare_Samples.sh

--- src/step_3_Prepare_Samples.sh
+++ src/step_3_Prepare_Samples.sh
@@ -6,10 +6,10 @@
 mkdir -p ${RES}/INPUT_STEP_2
 mkdir -p ${RES}/TEMP
 
-for INDIV in `awk '{print $2}' ${RES}/inds.tsv | sort | uniq`
+for INDIV in `awk '{print $3}' ${RES}/inds.tsv | sort | uniq`
 do
 	echo "	->" ${INDIV}
-	awk -v I=${INDIV} -v RES=${RES} '$2==I {print RES"/OUT_PROCESS/"$1"/"$2".R1.fq.gz"}' ${RES}/inds.tsv > ${RES}/TEMP/$$.R1
+	awk -v I=${INDIV} -v RES=${RES} '$3==I {print RES"/OUT_PROCESS/"$1"/"$2".R1.fq.gz"}' ${RES}/inds.tsv > ${RES}/TEMP/$$.R1
 	gzip -dc `tr "\n" " " < ${RES}/TEMP/$$.R1` > ${RES}/INPUT_STEP_2/${INDIV}.R1.fq
 	gzip -f ${RES}/INPUT_STEP_2/${INDIV}.R1.fq
 done
