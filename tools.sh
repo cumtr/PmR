fe812377792fc62d913c4500e883b6eb72c545c1
cumtr <tristan.cumer@gmail.com>
2018-02-19 17:37:54 +0100

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Path of the different programs used in PimpMyRad
+
+BBDUK=~/TOOLS/bbmap/bbduk.sh
+
+PROCESS_RADTAG=/usr/local/bin/process_radtags
+USTACKS=/usr/local/bin/ustacks
+CSTACKS=/usr/local/bin/cstacks
+SSTACKS=/usr/local/bin/sstacks
+POPULATIONS=/usr/local/bin/populations
