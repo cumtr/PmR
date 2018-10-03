d2e9f21ee6d5389a156ce0f4c2ce7c3bb8f3666e
Capblancq <thibaut.capblancq@gmail.com>
2018-10-03 17:23:41 +0200

    Update PmR.sh

--- PmR.sh
+++ PmR.sh
@@ -19,13 +19,13 @@ if [ "$1" == "-h" ]; then
 
   echo "Usage: `basename $0` [-h] [-s n] [-p paramfile.txt]
 
-Pimp My Rads: a program to perform assembly of RAD loci 
+Process My Rads: a program to perform assembly of RAD loci 
           where:
             -s (step)  perform the PmR pipeline according to step number 
                (all: perform all step)
             -p (params)  set the params file
 
-            * Documentation:  https://"
+            * Documentation:  https://github.com/cumtr/PmR"
   exit 0
 fi
 
@@ -45,7 +45,7 @@ fi
 
 echo ""  
 echo ""
-echo "Processing PimpMyRads program"
+echo "ProcessMyRads is running"
 echo ""
 
    if (( $step == 1 )); then
@@ -53,6 +53,8 @@ echo ""
       echo "############### Step 1 : Adapters trimming ###############"
       echo ""
       source `dirname $0`/src/step_1_filterReads.sh
+      echo ""
+      echo " Step 1 : OK"
       exit 0
 
    elif (( $step == 2 )); then
@@ -60,13 +62,17 @@ echo ""
       echo "############### Step 2 : Data demultiplexing and reads filtering ###############"
       echo ""
       source `dirname $0`/src/step_2_demultiplexing.sh
+      echo ""
+      echo " Step 2 : OK"
       exit 0
 
    elif (( $step == 3 )); then
       echo ""
       echo "############### Step 3 : Samples files preparation before loci reconstruction ###############"
       echo ""
-      source `dirname $0`/src/step_3_Prepare_Samples.sh
+      source `dirname $0`/src/step_3_Prepare_Samples.sh    
+      echo ""
+      echo " Step 3 : OK"
       exit 0
 
    elif (( $step == 4 )); then
@@ -74,6 +80,8 @@ echo ""
       echo "############### Step 4 : Range of M threshold for loci reconstruction ###############"
       echo ""
       source `dirname $0`/src/step_4_assembling_loci_gamme.sh
+      echo ""
+      echo " Step 4 : OK"
       exit 0
 
    elif (( $step == 5 )); then
@@ -81,6 +89,8 @@ echo ""
       echo "############### Step 5 : Loci reconstruction for each individuals using a M value of " ${M_CHOOSEN} " ###############"
       echo "" 
       source `dirname $0`/src/step_5_AssemblyStacks.sh
+      echo ""
+      echo " Step 5 : OK"
       exit 0
    
    elif (( $step == 6 )); then
@@ -88,6 +98,8 @@ echo ""
       echo "############### Step 6 : Build the catalog of loci ###############"
       echo ""
       source `dirname $0`/src/step_6_build_catalog.sh
+      echo ""
+      echo " Step 6 : OK"
       exit 0
 
    elif (( $step == 7 )); then
@@ -95,6 +107,8 @@ echo ""
       echo "############### Step 7 : Match of individual loci to the catalog ###############"
       echo ""
       source `dirname $0`/src/step_7_match_catalog.sh
+      echo ""
+      echo " Step 7 : OK"
       exit 0
 
    elif (( $step == 8 )); then
@@ -102,6 +116,8 @@ echo ""
       echo "############### Step 8 : Genetic dataset export ###############"
       echo ""
       source `dirname $0`/src/step_8_dataset_exportation.sh
+      echo ""
+      echo " Step 8: OK"
       exit 0
 
    elif (( $step == all )); then
@@ -138,6 +154,9 @@ echo ""
       echo "############### Step 8 : Genetic dataset export ###############"
       echo ""      
       source `dirname $0`/src/step_8_dataset_exportation.sh
+      echo ""
+      echo "Full PmR : OK"
+
       exit 0
 
    fi
